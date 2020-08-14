db7b2db34bad8cd27d1d74baf28595fcb827b9c1
gan0803 <gan0803@gmail.com>
2020-08-14 14:34:20 +0900

    Fixed using Bundle to receive the return value of getStringPreference()

--- app/src/main/java/gan0803/pj/contentresolverclient/MainActivity.kt
+++ app/src/main/java/gan0803/pj/contentresolverclient/MainActivity.kt
@@ -3,9 +3,14 @@ package gan0803.pj.contentresolverclient
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,9 +20,14 @@ class MainActivity : AppCompatActivity() {
         val auth = MyPreferences.AUTHORITY
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             cr.call(auth, MyPreferences.READ_PREFERENCE_METHOD, null, null)
+            val bundle = cr.call(auth, MyPreferences.GET_STRING_PREFERENCE_METHOD, null, null)
+            val str: String = bundle?.getString(MyPreferences.SAVED_STRING_KEY) ?: ""
+            Log.d(TAG, "get: $str")
         } else {
             cr.call(uri, MyPreferences.READ_PREFERENCE_METHOD, null, null)
+            val bundle = cr.call(uri, MyPreferences.GET_STRING_PREFERENCE_METHOD, null, null)
+            val str: String = bundle?.getString(MyPreferences.SAVED_STRING_KEY) ?: ""
+            Log.d(TAG, "get: $str")
         }
-        // TODO bundleでデータ受け渡し。戻り値でも行ける？
     }
 }
